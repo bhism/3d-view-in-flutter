93ae6b8372e321d4ac18c8e9d6316c66489f132e
BHISM <61289719+bhism@users.noreply.github.com>
2022-02-16 18:04:22 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,32 @@
+import 'package:babylonjs_viewer/babylonjs_viewer.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BabylonJSViewer(
+        src: 'assets/lion.glb',
+      ),
+    );
+  }
+}
